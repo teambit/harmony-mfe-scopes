d3601a63dc8e196f0e2863468266adc3aee05a6f
Josh Kuttler <yoshuakuttler@gmail.com>
2021-02-09 17:21:21 +0200

    add symphony url in create context

--- scopes/ui/graphql-context/graphql-context.tsx
+++ scopes/ui/graphql-context/graphql-context.tsx
@@ -1,6 +1,6 @@
 import React, { createContext } from 'react';
 
-export const gqlContext = createContext('');
+export const gqlContext = createContext('https://symphony.bit.dev/graphql');
 
 export type GqlProviderProps = {
   /**
