8f41c087a66a846ed3753a380fb52cc679763d3f
Josh Kuttler <yoshuakuttler@gmail.com>
2021-02-10 18:19:15 +0200

    export use-scope with compositions

--- scopes/ui/hooks/use-scopes/use-scope.compositions.tsx
+++ scopes/ui/hooks/use-scopes/use-scope.compositions.tsx
@@ -1,42 +1,42 @@
-import React from "react";
+import React from 'react';
 import {
-    GqlServerProvider,
-    useGqlServer,
-} from "@teambit/base-ui.hooks.use-graphql-light";
-import { useScopes } from "./use-scopes";
+  GqlServerProvider,
+  useGqlServer,
+} from '@teambit/base-ui.hooks.use-graphql-light';
+import { useScopes } from './use-scopes';
 
 export const Preview = () => {
-    const [scopes, loading, error] = useScopes(["teambit"]);
-    const serverUrl = useGqlServer();
+  const [scopes, loading, error] = useScopes(['teambit']);
+  const serverUrl = useGqlServer();
 
-    if (error)
-        return (
-            <div>
-                <div>(serverUrl: {serverUrl})</div>
-                <br />
-                <div>Error:</div>
-                <br />
-                <div>{error.toString()}</div>
-            </div>
-        );
-
-    if (loading) return <div>loading...</div>;
-
+  if (error)
     return (
-        <div>
-            <div>(serverUrl: {serverUrl})</div>
-            <br />
-            <div>Success!</div>
-            <br />
-            <div>{JSON.stringify(scopes)}</div>
-        </div>
+      <div>
+        <div>(serverUrl: {serverUrl})</div>
+        <br />
+        <div>Error:</div>
+        <br />
+        <div>{error.toString()}</div>
+      </div>
     );
+
+  if (loading) return <div>loading...</div>;
+
+  return (
+    <div>
+      <div>(serverUrl: {serverUrl})</div>
+      <br />
+      <div>Success!</div>
+      <br />
+      <div>{JSON.stringify(scopes)}</div>
+    </div>
+  );
 };
 
 export const WithExplicitContext = () => {
-    return (
-        <GqlServerProvider value="https://symphony.bit.dev/graphql">
-            <Preview />
-        </GqlServerProvider>
-    );
+  return (
+    <GqlServerProvider value="https://symphony.bit.dev/graphql">
+      <Preview />
+    </GqlServerProvider>
+  );
 };
